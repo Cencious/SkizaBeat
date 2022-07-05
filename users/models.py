c527f4d7b866c8482aa5f2f81a38db7d38215cec
Cencious <user.email>
2022-07-05 17:51:51 +0300

    add User model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=200)
+    email = models.EmailField(max_length=500)
+    password = models.CharField(max_length=200)
+    confirm_password = models.CharField(max_length=200)
