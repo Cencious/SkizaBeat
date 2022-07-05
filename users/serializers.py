a49c0bd25c0645ea9cf765230584a5902d0cf837
Cencious <user.email>
2022-07-05 17:05:08 +0300

    add kwargs to ensure passsword is write only and email is unique for every new user

--- users/serializers.py
+++ users/serializers.py
@@ -5,3 +5,15 @@ class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'password', 'email', 'first_name', 'last_name')
+        extra_kwargs = {
+            "password":{"write_only": True},
+            "email":{
+                "required": True,
+                "allow_blank": False,
+                "validators":[
+                    validators.UniqueValidator(
+                        User.objects.all(),"A user with that Email already exists"
+                    )
+                ]
+            }
+        }
