57130273eac4984a3e51a5fbf25c725eb3257d44
Cencious <user.email>
2022-07-05 14:11:54 +0300

    define the login view

--- users/views.py
+++ users/views.py
@@ -2,3 +2,8 @@ import imp
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.authtoken.serializers import AuthTokenSerializer
+
+@api_view(['POST'])
+def login_api(request):
+    serializer = AuthTokenSerializer(data=request.data)
+ 
