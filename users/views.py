bb695aece0802c2ab0ec8a7a4910ddae05c83c45
Cencious <user.email>
2022-07-05 14:19:10 +0300

    create token

--- users/views.py
+++ users/views.py
@@ -2,9 +2,11 @@ import imp
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.authtoken.serializers import AuthTokenSerializer
+from knox.auth import AuthToken
 
 @api_view(['POST'])
 def login_api(request):
     serializer = AuthTokenSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
     user = serializer.validated_data['user']
+    _,token = AuthToken.objects.create(user)
