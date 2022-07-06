2291a85dcd40e9abd248e23dfbd852d2f1c6f017
Cencious <user.email>
2022-07-06 21:31:48 +0300

    add userprofile serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers, validators
 
+from rest_framework.authtoken.models import Token
+
+from users import models
+
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
@@ -32,3 +36,40 @@ class RegisterSerializer(serializers.ModelSerializer):
         )
 
         return user
+
+
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.UserProfile
+        fields = ('id','email','name','password','profile_picture')
+        extra_kwargs = {'password':{'write_only':True}}
+
+    def create(self,validated_data):
+        '''Create and return a new user'''
+
+        user = models.UserProfile(
+            email =validated_data['email'],
+            name = validated_data['name']
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
+
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    '''for profile feed items'''
+
+    class Meta:
+        model=models.ProfileFeedItem
+        fields=('id','user_profile','status_text','created_on')
+        extra_kwargs={'user_profile':{'read_only':True}}
+
+
+
+
+
+
