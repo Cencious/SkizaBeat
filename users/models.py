6a00a15ff241d8310ab667f27de8c6f9bebd370f
Karenkoech <karenkoech3@gmail.com>
2022-07-06 13:55:05 -0400

    add userprofile models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,72 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+
 
 # Create your models here.
+class UserProfileManager(BaseUserManager):
+
+    def create_user(self,email,name,password=None):
+
+        if not email:
+            raise ValueError('Users must have an email address.')
+
+        email=self.normalize_email(email)
+        user=self.model(email=email,name=name)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+    
+    def create_superuser(self,email,name,password):
+
+        user=self.create_user(email,name,password)
+
+        user.is_superuser=True
+        user.is_staff=True
+
+        user.save(using=self._db)
+
+        return user
+
+class UserProfile(AbstractBaseUser,PermissionsMixin):
+    '''This is a userprofile class'''
+
+    email=models.EmailField(max_length=50,unique=True)
+    name=models.CharField(max_length=100)
+    profile_picture = CloudinaryField('pictures/',default='http://res.cloudinary.com/dim8pysls/image/upload/v1639001486/x3mgnqmbi73lten4ewzv.png')
+    is_active=models.BooleanField(default=True)
+    is_staff=models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+
+    USERNAME_FIELD ='email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        '''Get users full name'''
+
+        return self.name
+    
+    def get_short_name(self):
+        '''Get short name'''
+
+        return self.name
+
+    def __str__(self):
+        
+        return self.email
+
+class ProfileFeedItem(models.Model):
+
+    user_profile = models.ForeignKey('UserProfile',on_delete=models.CASCADE)
+    status_text=models.CharField(max_length=100)
+    created_on=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        
+        return self.status_text
